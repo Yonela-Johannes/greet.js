61d51401257f32ad1a9f19bac3b1e0c5a167a2da
Yonela-Johannes <johannesyonela@gmail.com>
2022-07-05 12:44:23 +0200

    background color

--- greet.js
+++ greet.js
@@ -1,4 +1,8 @@
-export const greet = (name) => 'Hello' + ' '+ name
+
+import chalk from 'chalk'
+
+export const greet = (name) => 'Hello' + ' ' + name
 
 export let message = greet('Xola')
+export let styledMessage = chalk.bgGreen.black(greet('Xola'))
 
--- nodeModules.js
+++ nodeModules.js
@@ -1,3 +1,5 @@
-import { message } from './greet.js'
+import { message, styledMessage } from './greet.js'
 
 console.log(message)
+
+console.log(styledMessage)
